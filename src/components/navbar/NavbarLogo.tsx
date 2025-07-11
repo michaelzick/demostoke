88320932bf575b778e14bd024d6fbe9763cdd363
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-07-11 14:08:16 -0700

    Improve SEO and accessibility

--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NavbarLogo = () => {
   return (
     <Link to="/" className="flex items-center gap-2">
-      <img src="/img/demostoke-logo-ds-transparent-cropped.webp" alt="Marker Icon" className="w-10 h-10" />
+      <img src="/img/demostoke-logo-ds-transparent-cropped.webp" alt="DemoStoke logo" className="w-10 h-10" />
       <span className="text-3xl font-bold" style={{ fontFamily: 'Tahoma, sans-serif', color: 'hsl(186 100% 48%)' }}>DemoStoke</span>
     </Link>
   );
