317b867c6c2941bc61fe2621640b7e5a8f188dab
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-07-17 06:51:42 -0700

    Replaced meta and poster img url

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -63,7 +63,7 @@ const HeroSection = () => {
               loop
               muted
               playsInline
-              poster="https://images.unsplash.com/photo-1617939533073-6c94c709370c?q=80&w=3544&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              poster="https://images.unsplash.com/photo-1590461283969-47fedf408cfd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               style={{ width: '100%', height: '100%', objectFit: 'cover' }}
             >
               <source src={bg.url} type="video/mp4" />
