08c007d3f55e185eff4751d82aae7f8d9d9e3cca
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2026-03-17 17:37:01 -0700

    fixed cf whitelist error

--- server/index.js
+++ server/index.js
@@ -610,12 +610,12 @@ app.use((req, res, next) => {
     'Referrer-Policy': 'strict-origin-when-cross-origin',
     'Content-Security-Policy': `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://cdn.amplitude.com https://api2.amplitude.com https://*.amplitude.com https://cdn.gpteng.co https://*.mapbox.com https://hcaptcha.com https://*.hcaptcha.com https://js.hcaptcha.com blob:;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://cdn.amplitude.com https://api2.amplitude.com https://*.amplitude.com https://cdn.gpteng.co https://*.mapbox.com https://hcaptcha.com https://*.hcaptcha.com https://js.hcaptcha.com https://static.cloudflareinsights.com blob:;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.amplitude.com;
       font-src 'self' https://fonts.gstatic.com https://*.amplitude.com;
       img-src 'self' data: https: http: https://*.mapbox.com https://*.amplitude.com;
       media-src 'self' https: http:;
-      connect-src 'self' https://qtlhqsqanbxgfbcjigrl.supabase.co https://api2.amplitude.com https://sr-client-cfg.amplitude.com https://cdn.amplitude.com https://api-sr.amplitude.com https://*.amplitude.com https://www.google-analytics.com https://www.googletagmanager.com https://analytics.google.com https://api.mapbox.com https://events.mapbox.com https://*.tiles.mapbox.com https://*.mapbox.com https://cdn.gpteng.co https://hcaptcha.com https://*.hcaptcha.com https://js.hcaptcha.com;
+      connect-src 'self' https://qtlhqsqanbxgfbcjigrl.supabase.co https://api2.amplitude.com https://sr-client-cfg.amplitude.com https://cdn.amplitude.com https://api-sr.amplitude.com https://*.amplitude.com https://www.google-analytics.com https://www.googletagmanager.com https://analytics.google.com https://api.mapbox.com https://events.mapbox.com https://*.tiles.mapbox.com https://*.mapbox.com https://cdn.gpteng.co https://hcaptcha.com https://*.hcaptcha.com https://js.hcaptcha.com https://cloudflareinsights.com;
       worker-src 'self' blob:;
       child-src 'self' blob: https://hcaptcha.com https://*.hcaptcha.com https://www.youtube.com;
       frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com https://www.youtube.com;
